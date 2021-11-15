b61c579f0d55d4d5020dfd08e0b75be23f028b17
Izzy Youngs <60447554+izzyoungs@users.noreply.github.com>
2021-11-14 20:20:31 -0500

    Update mapbox

--- Maps/R/Wolf-lines.R
+++ Maps/R/Wolf-lines.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(sf)
+library(rgdal)
 library(sfheaders)
 
 dat <- readOGR( 
@@ -16,6 +17,20 @@ dat3 <- sf_to_df(dat2, fill = TRUE )
 
 final_dat <- dat3 %>%
   mutate(string = paste0("[", x, ", ", y, "],")) %>%
-  select(string)
+  select(string) %>%
+  slice(which(row_number() %% 65 == 1))
+
+camera <- dat3 %>% 
+  mutate(x = x + .1, 
+         y = y + .1,
+         string = paste0("[", x, ", ", y, "],")) %>%
+  select(string) %>%
+  slice(which(row_number() %% 65 == 1))
+  
+
+
 
 write_csv(final_dat, "Output/wolf-route.csv")
+
+write_csv(camera, "Output/wolf-route-camera.csv")
+
